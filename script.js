a051ffd66843018e1f6252150ef06f3f15fa4fa8
bearagones <baragones@mail.sfsu.edu>
2024-10-10 23:08:25 -0700

    dark mode image
    uploaded image for dark mode + adjusted .js code

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function toggleDarkMode() {
     var element = document.body;
     var modeIcon = document.getElementById("modeIcon").querySelector("i");
+    var headshot = document.getElementById("beatrice-headshot");
 
     element.classList.toggle("dark-mode");
 
     if (element.classList.contains("dark-mode")) {
         modeIcon.classList.remove("fa-moon");
         modeIcon.classList.add("fa-sun");
+
+        headshot.src = "images/beatrice-dark.jpg";
     } else {
         modeIcon.classList.remove("fa-sun");
         modeIcon.classList.add("fa-moon");
+
+        headshot.src = "images/beatrice-light.jpeg";
     }
 }
